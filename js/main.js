bef83b6ffeedebee19d7b2a44630a196e270f3de
Jared Krinke <jared.krinke@gmail.com>
2019-07-19 16:10:59 -0700

    Round percents down to next 0.1% increment

--- js/main.js
+++ js/main.js
@@ -10,11 +10,38 @@
                 var input = fr.result;
                 var output = core.process(input, ',');
 
-                populateTable($('#output-names'), output.Names);
-                populateTable($('#output'), output.Data);
-                populateTable($('#output-invalid'), output.Invalid);
-                populateTable($('#output-raw'), output.DataRaw);
-                populateTable($('#output-invalid-raw'), output.InvalidRaw);
+                var renderHours = $('#output').DataTable.render.number( ',', '.', 2 );
+
+                // Round percents down to next 0.1% increment (to ensure that even 4.99% is inadequate, i.e. less than 5%)
+                var renderPercent = function (value) { return (Math.floor(value * 1000) / 10) + "%"; };
+
+                var summaryColumns = [
+                    { title: 'Name' },
+                    { title: 'Required Hours', render: renderHours },
+                    { title: 'Total', render: renderHours },
+                    { title: 'Supervised', render: renderHours },
+                    { title: 'Supervised, Individual', render: renderHours },
+                    { title: 'Fraction', render: renderPercent }
+                ];
+
+                var invalidColumns = [
+                    { title: 'Name' },
+                    { title: 'Individual' },
+                    { title: 'Duration', renderHours }
+                ];
+
+                populateTable(
+                    $('#output-names'),
+                    [
+                        { title: 'Name' },
+                        { title: 'Aliases' }
+                    ],
+                    output.Names);
+
+                populateTable($('#output'), summaryColumns, output.Data);
+                populateTable($('#output-invalid'), invalidColumns, output.Invalid);
+                populateTable($('#output-raw'), summaryColumns, output.DataRaw);
+                populateTable($('#output-invalid-raw'), invalidColumns, output.InvalidRaw);
             };
 
             fr.readAsText( file );
@@ -22,20 +49,8 @@
     });
 });
 
-function populateTable($, data) {
-    if (data && data.length > 0) {
-        var schema = [];
-        for (var key in data[0]) {
-            schema.push(key);
-        }
-
-        var columns = schema.map(function (key) {
-            return {
-                title: key,
-                render: $.DataTable.render.number( ',', '.', 2 )
-            };
-        });
-
+function populateTable($, columns, data) {
+    if (columns && data && data.length > 0) {
         // Align numbers to the right
         if (data.length > 1) {
             var firstRow = data[1];
@@ -50,7 +65,7 @@ function populateTable($, data) {
         var dataSet = [];
         for (var i = 0, count = data.length; i < count; i++) {
             var row = data[i];
-            dataSet.push(schema.map(function (key) { return row[key]; }));
+            dataSet.push(columns.map(function (column) { return row[column.title]; }));
         }
 
         $.DataTable({
